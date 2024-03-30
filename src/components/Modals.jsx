d36a6d89264cc8fd70b39b3d1e7eabe5e723a5c9
steffan-chyzak <steffan.chyzak@cambridgeonlinelearning.com>
2024-03-30 13:23:25 +0000

    Updated.

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -1,28 +1,45 @@
-export default ({ member, handleClose }) => {
-  return (
-    <dialog id="modal-member" open>
-      <article>
-        <a href="#close"
-          aria-label="Close"
-          className="close"
-          data-target="modal-member"
-          onClick={handleClose}
-        ></a>
-        <hgroup>
-          <div style={{
-            display: "flex",
-            gap: '1rem'
-          }}>
-            <img style={{ width: '200px' }}
-              src={`images/${member.slug}.svg`}
-              alt={member.name} />
-            <hgroup>
-              <h1>{member.name}</h1>
-              <p>{member.bio}</p>
-            </hgroup>
-          </div>
-        </hgroup>
-      </article>
-    </dialog>
-  )
-}
+export default ({ member, handleClose, handleChange }) => {
+	return (
+		<dialog id="modal-member" open>
+			<article>
+				<a href="#close" aria-label="Close" className="close" data-target="modal-member" onClick={handleClose}></a>
+				<hgroup>
+					<div
+						style={{
+							display: "flex",
+							gap: "1rem",
+						}}
+					>
+						<img style={{ width: "200px" }} src={`images/${member.slug}.svg`} alt={member.name} />
+						<hgroup>
+							<h1>{member.name}</h1>
+							<p>{member.bio}</p>
+							<hgroup>
+								<a
+									className="outline"
+									href="#"
+									role="button"
+									onClick={() => {
+										handleChange(Number(member.id) - 1);
+									}}
+								>
+									pevious
+								</a>
+								<a
+									className="outline"
+									href="#"
+									role="button"
+									onClick={() => {
+										handleChange(Number(member.id) + 1);
+									}}
+								>
+									next
+								</a>
+							</hgroup>
+						</hgroup>
+					</div>
+				</hgroup>
+			</article>
+		</dialog>
+	);
+};
