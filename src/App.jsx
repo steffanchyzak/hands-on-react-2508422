6598409b3a2aebec929776cc10687f3042895890
steffan-chyzak <steffan.chyzak@cambridgeonlinelearning.com>
2024-03-30 12:41:18 +0000

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,33 @@
-import React, { Component } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
-import ListCast from './components/ListCast';
+import React, { Component, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
 
 function App() {
-  const name = 'StarGazers'
-  return (
-    <div className="container">
-      <article>
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast />
-          <Support />
-        </hgroup>
-      </article>
-    </div>
-  )
+	const name = "StarGazers";
+	let [memberInfo, setMemberInfo] = useState(null);
+	return (
+		<div className="container">
+			<hgroup>
+				<img src="images/group.svg" alt="StarGazers Group" />
+				<h1>Meet the StarGazers</h1>
+				<p>
+					Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.
+				</p>
+				<ListCast
+					onChoice={(info) => {
+						setMemberInfo(info);
+					}}
+				/>
+				{memberInfo && (
+					<Modals
+						member={memberInfo}
+						handleClose={() => {
+							setMemberInfo(null);
+						}}
+					/>
+				)}
+			</hgroup>
+		</div>
+	);
 }
-export default App
+export default App;
