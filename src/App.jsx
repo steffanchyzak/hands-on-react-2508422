f517fbd82b76860febcb705e33f3ac3e4febd4ba
steffan-chyzak <steffan.chyzak@cambridgeonlinelearning.com>
2024-03-30 13:07:14 +0000

    Fixed menu to use dropdown from pico css docs

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import ListCast from './components/ListCast';
-import Modals from './components/Modals';
+import React, { useEffect, useState } from "react";
+import ListCast from "./components/ListCast";
+import Modals from "./components/Modals";
+import Nav from "./components/Nav";
 
 function App() {
-  const name = 'StarGazers'
-  const [cast, setCast] = useState([]);
-  let [memberInfo, setMemberInfo] = useState(null);
+	const [cast, setCast] = useState([]);
+	let [memberInfo, setMemberInfo] = useState(null);
 
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
-  }
+	async function fetchCast() {
+		const response = await fetch("cast.json");
+		setCast(await response.json());
+	}
 
-  useEffect(() => {
-    fetchCast();
-  });
+	useEffect(() => {
+		fetchCast();
+	});
 
-  return (
-    <div className="container">
-      <hgroup>
-        <img src="images/group.svg" alt="StarGazers Group" />
-        <h1>Meet the Stargazers</h1>
-        <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-        <ListCast cast={cast} onChoice={(info) => { setMemberInfo(info) }} />
-        {memberInfo && <Modals member={memberInfo} handleClose={() => { setMemberInfo(null) }} />}
-      </hgroup>
-    </div>
-  )
+	return (
+		<>
+			<Nav
+				cast={cast}
+				onChoice={(info) => {
+					setMemberInfo(info);
+				}}
+			/>
+			<div className="container">
+				<hgroup>
+					<img src="images/group.svg" alt="StarGazers Group" />
+					<h1>Meet the Stargazers</h1>
+					<p>
+						Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.
+					</p>
+					<ListCast
+						cast={cast}
+						onChoice={(info) => {
+							setMemberInfo(info);
+						}}
+					/>
+					{memberInfo && (
+						<Modals
+							member={memberInfo}
+							handleClose={() => {
+								setMemberInfo(null);
+							}}
+						/>
+					)}
+				</hgroup>
+			</div>
+		</>
+	);
 }
-export default App
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+export default ({ cast, onChoice }) => {
+	return (
+		<nav className="container">
+			<ul>
+				<li>
+					<details class="dropdown">
+						<summary aria-haspopup="listbox" role="link">
+							Cast
+						</summary>
+						<ul role="listbox">
+							{cast.map((member) => (
+								<li key={member.id}>
+									<a
+										onClick={() => {
+											onChoice(member);
+										}}
+										data-tooltip={member.name}
+									>
+										{member.name}
+									</a>
+								</li>
+							))}
+						</ul>
+					</details>
+				</li>
+			</ul>
+			<ul>
+				<li>
+					<a href="#">
+						<img style={{ height: "50px" }} src="images/logo_bug_stargazers.svg" alt="stargazer logo" />
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+};
